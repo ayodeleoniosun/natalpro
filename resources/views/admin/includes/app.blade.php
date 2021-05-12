e41b7d0df3b70b79a49f2e609b016581bede788e
Dev-AY <heywire4u@gmail.com>
2021-05-12 18:50:39 +0100

    chore: add insecure requests

--- resources/views/admin/includes/app.blade.php
+++ resources/views/admin/includes/app.blade.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Session;
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
+         <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="">
         <meta name="csrf-token" content="{{ csrf_token() }}" />
