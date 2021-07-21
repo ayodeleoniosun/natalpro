6d74c840791ee70c7f017133981cbb5abadb7483
Dev-AY <heywire4u@gmail.com>
2021-07-21 02:50:33 +0100

    chore: update reminder command to run at 2pm daily

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('vaccination:reminder')->dailyAt('12:30');
+        $schedule->command('vaccination:reminder')->dailyAt('14:00');
     }
 
     /**
