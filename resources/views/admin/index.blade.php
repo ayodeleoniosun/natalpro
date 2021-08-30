4b03dc10be20c63f876757c7a41136380f253e13
Ayodele <ayodele@ogunlabs.com>
2021-08-30 12:01:33 +0100

    test: datadog

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -64,6 +64,10 @@
         </div>
     </section>
     
+    <script type="text/javascript">
+        console.log(window._DATADOG_SYNTHETICS_BROWSER);
+    </script>
+
     <script src="{{ URL::asset('assets/plugins/bootstrap/js/bootstrap.min.js') }}"></script>
 </body>
 
